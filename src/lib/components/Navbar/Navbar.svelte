0f621004321b6e3e4f233c486a73396ec0b226e2
Vladyslav Pavlenko <vladyslavpavlen@gmail.com>
2024-08-24 19:35:11 +0300

    feat: add navbar component

---
+++ src/lib/components/Navbar/Navbar.svelte
@@ -0,0 +1,28 @@
+<script>
+	import {
+		Navbar,
+		NavBrand,
+		NavUl,
+		NavHamburger,
+		NavLi,
+		Button,
+	} from 'flowbite-svelte';
+</script>
+
+<Navbar color="form" class="sticky top-6 rounded-2xl z-50 backdrop-filter backdrop-blur-md bg-opacity-90 border border-gray-200">
+	<NavBrand href="/">
+		<img src="/src/lib/images/boardly-logo.svg" class="me-3 m-1 h-9 sm:h-9" alt="Boardly Logo" />
+		<span class="hidden md:inline self-center whitespace-nowrap text-2xl font-medium dark:text-white">boardly</span>
+	</NavBrand>
+	<div class="flex md:order-2">
+		<div class="flex gap-4">
+			<Button href="/login" size="sm" color="none" class="text-lg border-0 hover:underline underline-offset-4 hover:text-current">Log in</Button>
+			<Button href="/signup" class="text-lg" color="dark">Sign up</Button>
+		</div>
+		<NavHamburger />
+	</div>
+	<NavUl class="order-1">
+		<NavLi href="/about" class="hover:underline underline-offset-4 text-lg hover:text-current">About</NavLi>
+		<NavLi href="/explore" class="hover:underline underline-offset-4 text-lg hover:text-current">Explore</NavLi>
+	</NavUl>
+</Navbar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,15 @@
 <script>
-	import Header from './Header.svelte';
+	import Header from '$lib/components/Navbar/Navbar.svelte';
 	import '../app.css';
+	import { Footer } from 'flowbite-svelte';
 </script>
 
-<div class="app">
+<div class="m-6 font-sans">
 	<Header />
 
 	<main>
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	<Footer />
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
