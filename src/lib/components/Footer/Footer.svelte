d2ee19c08dad643416caac7c7c8d219c82c8ae52
Vladyslav Pavlenko <vladyslavpavlen@gmail.com>
2024-08-25 03:38:42 +0300

    feat: create footer component

---
+++ src/lib/components/Footer/Footer.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Footer } from 'flowbite-svelte';
+</script>
+
+<nav class="p-6">
+	<Footer class="p-6 text-center mt-16">
+		<p>&copy; 2024 Boardly. All rights reserved.</p>
+		<nav class="flex justify-center gap-6 mt-4">
+			<a href="/privacy" class="hover:underline underline-offset-2">Privacy Policy</a>
+			<a href="/terms" class="hover:underline underline-offset-2">Terms of Service</a>
+		</nav>
+	</Footer>
+</nav>
