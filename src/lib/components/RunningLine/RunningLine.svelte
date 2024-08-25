0bb9c4edbed652fce2ccf6bc200c17e99c0fa472
Vladyslav Pavlenko <vladyslavpavlen@gmail.com>
2024-08-25 03:39:42 +0300

    feat: add log in & sign up pages

---
+++ src/lib/components/RunningLine/RunningLine.svelte
@@ -0,0 +1,58 @@
+<script>
+	export let speed = 65;
+	export let customClass = '';
+	export let customStyle = '';
+
+	let quotes = [
+		{ text: `"It's not whether you win or lose, it's how you play the game."`, author: "Anonymous" },
+		{ text: `"When you play the game of thrones, you win or you die."`, author: "Cersei Lannister, Game of Thrones" },
+		{ text: `"Life is a game. All you have to do is know how to play it."`, author: "Albert Einstein" },
+		{ text: `"In the game of life, the rules keep changing."`, author: "Anonymous" },
+		{ text: `"The dice are waiting for your roll!"`, author: "Anonymous" },
+		{ text: `"Strategy without tactics is the slowest route to victory."`, author: "Sun Tzu" },
+	];
+
+	function shuffle(array) {
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+		return array;
+	}
+
+	quotes = shuffle(quotes);
+</script>
+
+<div class={`w-full bg-gray-900 text-white py-2 overflow-hidden relative ${customClass}`} style={customStyle}>
+	<div class="running-line flex items-center space-x-8 marquee-content" style="animation-duration: {speed}s;">
+		{#each quotes as { text, author }}
+			<span>{text} - {author}</span>
+		{/each}
+
+		{#each quotes as { text, author }}
+			<span>{text} - {author}</span>
+		{/each}
+	</div>
+</div>
+
+<style>
+    @keyframes marquee {
+        0% {
+            transform: translateX(0%);
+        }
+        100% {
+            transform: translateX(-100%);
+        }
+    }
+
+    .running-line {
+        display: flex;
+        width: 200%;
+        animation: marquee linear infinite;
+    }
+
+    .marquee-content {
+        display: flex;
+        white-space: nowrap;
+    }
+</style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,36 @@
+<svelte:head>
+	<title>Log in</title>
+	<meta name="description" content="Log into Boardly" />
+</svelte:head>
+
+<script lang="ts">
+	import { Section } from 'flowbite-svelte-blocks';
+	import { Button, Label, Input } from 'flowbite-svelte';
+</script>
+
+<div class="text flex flex-col items-center justify-center h-[740px]">
+	<Section name="login">
+			<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+				<form class="flex flex-col space-y-3" action="/">
+					<img src="/src/lib/images/boardly-logo.svg" class="me-3 m-1 sm:h-16" alt="Boardly Logo" />
+					<h3 class="text-center text-xl font-medium text-gray-900 dark:text-white p-0">Log in</h3>
+					<Label class="space-y-2 text-lg">
+						<span>Email</span>
+						<Input size="lg" class="font-normal" type="email" name="email" placeholder="name@example.com" required />
+					</Label>
+					<Label class="space-y-2 text-lg">
+						<span>Password</span>
+						<Input size="lg" class="font-normal" type="password" name="password" placeholder="••••••••" required />
+					</Label>
+					<Button type="submit" size="lg" class="w-full">Log in</Button>
+					<div class="w-1/5 place-self-center py-2">
+						<hr>
+					</div>
+					<Button type="submit" size="lg" color="light" class="w-full">Continue with Google</Button>
+					<p class="text-lg text-center font-light text-gray-500 dark:text-gray-400">
+						Don’t have an account yet? <a href="/signup" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+					</p>
+				</form>
+			</div>
+	</Section>
+</div>
